ff58370765c6969933472c37b9f65ee5ccf2dc0b
Nicolas Lloyd <nicolas.lloyd2935@gmail.com>
2021-03-25 22:12:10 -0400

    Implemented Prisma
    Implemented Prisma into the backend to simplify the connection to and querying  of our MySQL database. Currently using temporary API endpoints for testing.

--- server/index.js
+++ server/index.js
@@ -1,10 +1,29 @@
+var { PrismaClient } = require("@prisma/client");
+var config = require("./config");
+var express = require("express");
+var app = express();
+var apiRouter = express.Router();
 
-var config = require('./config');
-var express = require('express');
-var app  = express();
+const prisma = new PrismaClient();
 
+apiRouter.get("/sessions", async (req, res) => {
+  const allSessions = await prisma.session.findMany();
+  console.log(JSON.stringify(allSessions));
+  res.json(allSessions);
+});
 
+apiRouter.post("/addSession", async (req, res) => {
+  const { sessionId } = req.body;
+  const result = await prisma.session.create({
+    data: {
+      sessionId,
+    },
+  });
+  res.json(result);
+});
+
+app.use("/api", apiRouter);
 
 app.listen(config.port, () => {
-    console.log("Server is running on port 8080");
+  console.log("Server is running on port 8080");
 });
