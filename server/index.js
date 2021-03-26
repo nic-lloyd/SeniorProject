398a54ffce6f9faccef6e8114d4cdbc4a394c988
Nicolas Lloyd <nicolas.lloyd2935@gmail.com>
2021-03-26 19:52:13 -0400

    Added more endpoints
    Added GET, POST, and PUT endpoints to manipulate the user table in the database

--- server/index.js
+++ server/index.js
@@ -12,6 +12,7 @@ app.use(express.json());
  */
 apiRouter.get("/sessions", async (req, res) => {
   const allSessions = await prisma.session.findMany(); //get all rows from the session table
+
   console.log(new Date(Date.now()).toISOString()); //log timestamp of the API call
   console.log(JSON.stringify(allSessions)); //log result
   res.json(allSessions); //return result as a JSON
@@ -27,9 +28,22 @@ apiRouter.get("/sessions/:id", async (req, res) => {
       sessionId: id,
     },
   });
+
   console.log(new Date(Date.now()).toISOString()); //log timestamp of the API call
   console.log(JSON.stringify(session));
   res.json(session);
+});
+
+/**
+ * Add a user to a given
+ */
+apiRouter.get("/users/:sessionId", async (req, res) => {
+  const { id } = req.params;
+  const result = await prisma.sessionUsers.findMany({
+    where: { sessionId: id },
+  });
+  console.log(result.length + " records found.");
+  res.json(result);
 });
 
 /**
@@ -46,6 +60,34 @@ apiRouter.post("/addSession", async (req, res) => {
   });
   console.log(new Date(Date.now()).toISOString()); //log timestamp of the API call
   res.json(result);
+});
+
+/**
+ * Add user to the database. req.body should contain a user id
+ * and a screen name
+ */
+apiRouter.post("/addUser", async (req, res) => {
+  const { userId, name } = req.body;
+  const newUser = await prisma.user.create({
+    data: {
+      userId: userId,
+      name: name,
+    },
+  });
+  console.log("New user created");
+  res.json(newUser);
+});
+
+/**
+ * PUT request to update a single user's display name
+ */
+apiRouter.put("/updateName/:id", async (req, res) => {
+  const userId = req.params.id;
+  const user = await prisma.user.update({
+    where: { userId: userId },
+    data: { name: req.body.name },
+  });
+  res.json(user);
 });
 
 app.use("/api", apiRouter);
