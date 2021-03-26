18a03c71561c6ef38ad26aebc384461b0146ac2a
Nicolas Lloyd <nicolas.lloyd2935@gmail.com>
2021-03-26 04:20:55 -0400

    Solved issue with POST requests
    issue with POST requests was coming from improper request formatting from the client-side, not from the construction of the REST API

--- server/index.js
+++ server/index.js
@@ -7,12 +7,19 @@ var apiRouter = express.Router();
 const prisma = new PrismaClient();
 app.use(express.json());
 
+/**
+ * GET request for all sessions in the session table
+ */
 apiRouter.get("/sessions", async (req, res) => {
-  const allSessions = await prisma.session.findMany();
-  console.log(JSON.stringify(allSessions));
-  res.json(allSessions);
+  const allSessions = await prisma.session.findMany(); //get all rows from the session table
+  console.log(new Date(Date.now()).toISOString()); //log timestamp of the API call
+  console.log(JSON.stringify(allSessions)); //log result
+  res.json(allSessions); //return result as a JSON
 });
 
+/**
+ * GET request for a single session in the session table
+ */
 apiRouter.get("/sessions/:id", async (req, res) => {
   const { id } = req.params;
   const session = await prisma.session.findUnique({
@@ -20,17 +27,24 @@ apiRouter.get("/sessions/:id", async (req, res) => {
       sessionId: id,
     },
   });
+  console.log(new Date(Date.now()).toISOString()); //log timestamp of the API call
   console.log(JSON.stringify(session));
   res.json(session);
 });
 
+/**
+ * POST request to add a session to the session table with the
+ * current timestamp
+ */
 apiRouter.post("/addSession", async (req, res) => {
-  const { id } = req.body;
+  const id = req.query.sessionId;
   const result = await prisma.session.create({
     data: {
       sessionId: id,
+      dateCreated: new Date(Date.now()).toISOString(),
     },
   });
+  console.log(new Date(Date.now()).toISOString()); //log timestamp of the API call
   res.json(result);
 });
 
